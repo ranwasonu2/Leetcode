1ddb1b55d244014c3699a0b5c7051dde034153fb
ranwasonu2 <ranwasonu2@gmail.com>
2026-07-26 15:43:52 +0530

    Time: 75 ms (27.61%), Space: 111.5 MB (14.09%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.cpp
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -3,9 +3,9 @@ public:
     bool containsDuplicate(vector<int>& nums) {
         unordered_set<int>s;
         for(int i=0;i<nums.size();i++){
-            if(s.contains(nums[i])){
+            if(s.find(nums[i])!=s.end()){
 
-                //s.find(nums[i]!=s.end())
+                
                 return true;
             }
             s.insert(nums[i]);
